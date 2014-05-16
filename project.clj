4632da5f52942fe6b154e97fedb50ac3905460a7
John Wiseman <jjwiseman@gmail.com>
2014-05-16 14:37:07 -0700

    Added :license to project.clj.
    This makes it easier to audit licenses with tools like lein-licenses.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject net.cgrand/regex "1.1.0"
   :description "a DSL for people who prefer verbose, composable regexes"
   :url "http://github.com/cgrand/regex/"
+  :license {:name "Eclipse Public License 1.0"}
   :dependencies [[org.clojure/clojure "[1.2.0,)"]])
